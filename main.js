6bd0d4d7921234eb7f6de626eca84b935bc148aa
Ali Muhammad <81983704+Ali-muhammed7040@users.noreply.github.com>
2024-11-27 12:55:50 +0500

    sum of n

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+// sum of n
+//if n = 4
+// 4+3+2+1+0
+
+const n = 20;
+let sum = 0;
+for (let i = 0; i < n + 1; i++) {
+  sum += i;
+}
+
+console.log(sum);
---
+++ script.js
@@ -0,0 +1,11 @@
+export let friendName = "Ali";
+export let friendName2 = "awais";
+
+const myName = "Saif";
+
+export default myName;
+/// export 2 types
+
+// 1) default export only one in one file
+
+//  2  name export can be multiple in one file
